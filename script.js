cb11cdb8dc5a1939bb3eec822a960db9be6bac2d
Jonas-Kodehode <152984463+Jonas-Kodehode@users.noreply.github.com>
2024-03-08 14:57:02 +0100

    Almost done

---
+++ script.js
@@ -0,0 +1,44 @@
+// Dom Elements
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links"); // Changed to target .nav-links
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+// Adds active class to the section you are on
+navLinks[0].classList.add("active");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.href.includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
+// Picture carousel
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
